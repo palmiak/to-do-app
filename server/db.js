2b424c3bf920be2dfcf58184b446151a6bb2f9e4
Maciek Palmowski <m.palmowski@freshpixels.pl>
2022-11-30 09:39:54 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 require('dotenv').config()
 
 const pool = new Pool({
-    user: "aniakubow",
+    user: proccess.env.USERNAME,
     password: process.env.PASSWORD,
     host: "localhost",
     port: 5432,
